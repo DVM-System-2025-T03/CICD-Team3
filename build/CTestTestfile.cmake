bafd4203effcfb13e759618cd0250c821878aea7
K-da-fire <tmddnr3503@gmail.com>
2025-05-14 09:25:54 +0900

    Remove build directory from repository

--- build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/tmddnr3503/workspace/dvm/CICD/CICD-Team3
-# Build directory: /Users/tmddnr3503/workspace/dvm/CICD/CICD-Team3/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("_deps/googletest-build")

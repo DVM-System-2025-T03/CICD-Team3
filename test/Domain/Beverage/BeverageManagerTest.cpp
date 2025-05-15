588b6fa5e66710528a0cfde522f7fd862a0fce08
K-da-fire <tmddnr3503@gmail.com>
2025-05-16 01:09:58 +0900

    main 원복

--- test/Domain/Beverage/BeverageManagerTest.cpp
+++ test/Domain/Beverage/BeverageManagerTest.cpp
@@ -16,23 +16,23 @@ protected:
     }
 };
 
-TEST_F(BeverageManagerTest, GetBeverage_Found_ReturnsBeverage) {
+TEST_F(BeverageManagerTest, GetBeverage_성공_Beverage반환) {
     Beverage result = manager.getBeverage(1);
     EXPECT_EQ(result.getId(), 1);
 }
 
-TEST_F(BeverageManagerTest, GetBeverage_NotFound_CharInput_ThrowsException) {
+TEST_F(BeverageManagerTest, GetBeverage_실패_문자입력_예외) {
     EXPECT_THROW(manager.getBeverage('a'), NotFoundException);
 }
 
-TEST_F(BeverageManagerTest, GetBeverage_NotFound_NegativeInput_ThrowsException) {
+TEST_F(BeverageManagerTest, GetBeverage_실패_음수입력_예외) {
     EXPECT_THROW(manager.getBeverage(-1), NotFoundException);
 }
 
-TEST_F(BeverageManagerTest, GetBeverage_NotFound_ZeroInput_ThrowsException) {
+TEST_F(BeverageManagerTest, GetBeverage_실패_0입력_예외) {
     EXPECT_THROW(manager.getBeverage(0), NotFoundException);
 }
 
-TEST_F(BeverageManagerTest, GetBeverage_NotFound_OverMaxInput_ThrowsException) {
+TEST_F(BeverageManagerTest, GetBeverage_실패_초과입력_예외) {
     EXPECT_THROW(manager.getBeverage(999), NotFoundException);
 }

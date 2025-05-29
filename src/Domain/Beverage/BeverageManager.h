33b4fe80d188327f064b0ea22c08b892043ae953
seongjunnoh <joon1754@naver.com>
2025-05-29 13:21:10 +0900

    test : RequestPaymentController 테스트 코드 작성

--- src/Domain/Beverage/BeverageManager.h
+++ src/Domain/Beverage/BeverageManager.h
@@ -11,7 +11,7 @@ public:
     BeverageManager() = default;
     virtual bool hasEnoughStock(int beverageId, int quantity);
     virtual bool reduceQuantity(int beverageId, int quantity);
-    Beverage getBeverage(int beverageId);
+    virtual Beverage getBeverage(int beverageId);
     void addBeverage(Beverage beverage);
     int getStock(int beverageId);
 };
